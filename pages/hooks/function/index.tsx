56f7a471a2aad017bc1c31d9ce6f649f806ff0cf
Khoi Nguyen <khoinguyenvk9@gmail.com>
2021-07-07 09:50:03 +0700

    add react bootstrap, life cycle example

--- pages/hooks/function/index.tsx
+++ pages/hooks/function/index.tsx
@@ -6,20 +6,29 @@ import { MUIButton } from '../../../components/MUIButton'
 const LifeCycle = () => {
     const [count, setCount] = useState(0)
     const [isReal, setIsReal] = useState(true)
-
+    console.log("render is run...")
     // == componentDidMount & componentDidUpdate
-    useEffect(() => {
-        console.log("hello");
-        // componentWillUnMount
-        return () => {
-            console.log("close");
+    // useEffect(() => {
+    //     console.log("hello");
+    //     // componentWillUnMount
+    //     return () => {
+    //         console.log("close");
+    //     }
+    // })
 
-        }
-    })
+    // chay 1 lan duy nhat sau khi render
+    useEffect(() => {
+        console.log("hello world!");
+    }, [])
+
+    // chay khi count thay doi
+    useEffect(() => {
+        console.log("change count");
+    }, [count])
     return (
         <Container>
             <div>this is LifeCycle hook</div>
-            <Button onClick={() => { setIsReal(!isReal) }}>Click Me</Button>
+            <Button onClick={() => { setCount(count + 1) }}>Click Me</Button>
             <p>{count}</p>
 
             <MUIButton onClickButton={() => { setIsReal(!isReal) }} title="Click"></MUIButton>
